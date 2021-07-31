c7978ac35bda4cfa22db28db48abdbf3fc267cae
tomazvila <tomazvila@outlook.com>
2021-07-31 11:54:00 +0300

    Add download pdf route to api
    TODO: add url encoding. Route does not work yet, because of non existing
    url encoding

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ app = FastAPI()
 async def root():
     return {"message": "Hello world"}
 
+@app.get("/download_pdf/{web_page}")
+async def download(web_page: str):
+    file_location = "/tmp/html2pdf/" + web_page + ".pdf"
+    get_pdf_from_url(web_page, file_location)
+    return FileResponse(path=file_location)
+
 def get_pdf_from_url(url, location):
     HTML(url).write_pdf(location)
