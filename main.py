2d5f2c986f2163316912d6f13422b0087ab247a4
tomazvila <tomazvila@outlook.com>
2021-07-31 12:49:18 +0300

    Add /tmp/html2pdf clean route

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.responses import JSONResponse
 from weasyprint import HTML
 import os
 
@@ -16,6 +17,26 @@ async def download(web_page: str):
     file_location = "/tmp/html2pdf/" + file
     get_pdf_from_url(web_page, file_location)
     return FileResponse(path=file_location)
+
+@app.delete("/delete_tmp")
+def delete_tmp():
+    folder = '/tmp/html2pdf/'
+    try:
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        return JSONResponse(content={
+            "removed": True
+            }, status_code=200)   
+    except Exception as e:
+        return JSONResponse(content={
+            "removed": False,
+            "error_message": "Failed to clean /tmp/"
+        }, status_code=404)
+
 
 def get_pdf_from_url(url, location):
     HTML(url).write_pdf(location)
