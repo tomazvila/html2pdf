ac3950778576b45139e77d530635cb75c07f5c54
tomazvila <tomazvila@outlook.com>
2021-07-31 12:24:15 +0300

    Add url decoding
    Url strings should not be passed in plain text

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from weasyprint import HTML
+from urllib.parse import urlparse
 
 app = FastAPI()
 
@@ -7,8 +8,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello world"}
 
-@app.get("/download_pdf/{web_page}")
-async def download(web_page: str):
+@app.get("/download_pdf/{web_page_encoded:path}")
+async def download(web_page_encoded: str):
+    o = urlparse(web_page_encoded)
+    web_page = o.geturl()
     file_location = "/tmp/html2pdf/" + web_page + ".pdf"
     get_pdf_from_url(web_page, file_location)
     return FileResponse(path=file_location)
