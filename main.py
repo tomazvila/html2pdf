e889e1a0ffec224a298e0ed28d14c494d0bb0265
tomazvila <tomazvila@outlook.com>
2021-07-31 12:31:34 +0300

    Add check if tmp files directory exists already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from weasyprint import HTML
-from urllib.parse import urlparse
+import os
 
 app = FastAPI()
 
@@ -8,13 +8,18 @@ app = FastAPI()
 async def root():
     return {"message": "Hello world"}
 
-@app.get("/download_pdf/{web_page_encoded:path}")
-async def download(web_page_encoded: str):
-    o = urlparse(web_page_encoded)
-    web_page = o.geturl()
+@app.get("/download_pdf/{web_page:path}")
+async def download(web_page: str):
+    check_tmp_html2pdf_folder()
     file_location = "/tmp/html2pdf/" + web_page + ".pdf"
     get_pdf_from_url(web_page, file_location)
     return FileResponse(path=file_location)
 
 def get_pdf_from_url(url, location):
     HTML(url).write_pdf(location)
+
+def check_tmp_html2pdf_folder():
+    try:
+        os.makedirs("/tmp/html2pdf")
+    except FileExistsError:
+        pass
